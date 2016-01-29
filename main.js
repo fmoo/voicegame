d87ed4654cd95b964131d87354d01b759420833b
Peter Ruibal <ruibalp@gmail.com>
2016-01-29 10:11:56 -0800

    Make things a lot better

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 'strict';
 
-var React = require('react');
-var ReactDOM = require('react-dom');
-var _ = require('lodash');
+const arrayContains = require('array-contains');
+const React = require('react');
+const ReactDOM = require('react-dom');
+const ReactCSSTransitionGroup = require('react-addons-css-transition-group');
+const _ = require('lodash');
 
 
-const WORDS = [
+const SHAPES = [
   ["circle", "res/circle.png", []],
   ["square", "res/square.jpg", []],
   ["octagon", "res/octagon.png", []],
@@ -23,15 +25,57 @@ const WORDS = [
   ["triangle", "res/triangle.png", []],
 ]
 
+const GAMES = [
+  ["Shapes", SHAPES],
+]
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentGame: null,
+    };
+  }
+
+  _renderSelector() {
+    return (
+      <h1>
+        <a 
+          style={{
+            textDecoration: "none",
+          }}
+          href="#" onClick={(event) => {
+            this.setState({currentGame: SHAPES});
+          }}>
+          Shapes
+        </a>
+      </h1>
+    );
+  }
+
+  render() {
+    if (this.state.currentGame === null) {
+      return this._renderSelector();
+    }
+
+    return (
+      <Game cards={this.state.currentGame} />
+    );
+  }
+}
+
+
+class Game extends React.Component {
 
   constructor(props) {
     super(props);
+    this._onGuess = this._onGuess.bind(this);
+
     this.state = {
       currentWord: null,
     };
     this.state.currentWord = this._pickWord();
-    this._onGuess = this._onGuess.bind(this);
     this._speechRenderer = null;
   }
 
@@ -39,12 +83,18 @@ class App extends React.Component {
     return (
       <div style={{textAlign: "center"}} >
         <h1>What Shape is it?</h1>
-        <img 
-          style={{
-            width: "400px",
-          }}
-          src={this.state.currentWord[1]}
-        />
+        <div className="promptContainer">
+          <ReactCSSTransitionGroup
+            transitionName="promptImage"  
+            transitionEnterTimeout={1000}
+            transitionLeaveTimeout={1000}>
+            <img 
+              className="promptImage"
+              key={this.state.currentWord[0]}
+              src={this.state.currentWord[1]}
+            />
+          </ReactCSSTransitionGroup>
+        </div>
         <RenderSpeech
           onguess={this._onGuess}
           ref={(o) => {this._speechRenderer = o}}
@@ -54,20 +104,24 @@ class App extends React.Component {
   }
 
   _pickWord() {
-    let chosen = _.sample(WORDS);
+    let chosen = _.sample(this.props.cards);
     if (this.state.currentWord === null) {
       return chosen;
     }
 
     while (chosen[0] === this.state.currentWord[0]) {
-      chosen = _.sample(WORDS);
+      chosen = _.sample(this.props.cards);
     }
     return chosen; 
   }
 
   _onGuess(guess) {
     console.log("Guess: " + guess + "..." + this.state.currentWord[0]);
-    if (guess.toLowerCase().trim() === this.state.currentWord[0].toLowerCase().trim()) {
+    guess = guess.toLowerCase().trim();
+    if (
+      guess === this.state.currentWord[0] ||
+      arrayContains(this.state.currentWord[2], guess)
+    ) {
       console.log("Correct!")
       this.setState({
         currentWord: this._pickWord(),
@@ -83,10 +137,11 @@ class RenderSpeech extends React.Component {
 
   constructor(props) {
     super(props);
+    this._onSpeechResult = this._onSpeechResult.bind(this);
+
     this.initSpeech();
     this.state = {
       guess: '',
-      allGuesses: [],
     };
   }
 
@@ -95,11 +150,18 @@ class RenderSpeech extends React.Component {
   };
 
   render() {
+    if (this.speech === null) {
+      return this._renderUnsupportedBrowser();
+    }
     return (
       <div>
         <h2>{this.state.guess}</h2>
       </div>
     );
+  }
+
+  _renderUnsupportedBrowser() {
+    return <h1>Sorry, this browser is not supported.  Have you tried Chrome?</h1>;
   }
 
   clear() {
@@ -112,18 +174,33 @@ class RenderSpeech extends React.Component {
     });
   }
 
+  componentWillUnmount() {
+    if (this.speech !== null) {
+      this.speech.onend = null;
+      this.speech.stop();
+    }
+  }
+
+  _newSpeechRecognition() {
+    if (typeof SpeechRecognition !== "undefined") {
+      return new SpeechRecognition();
+    }
+    if (typeof webkitSpeechRecognition !== "undefined") {
+      return new webkitSpeechRecognition();
+    }
+    return null;
+  }
+
   initSpeech() {
-    this.speech = new webkitSpeechRecognition();
+    this.speech = this._newSpeechRecognition();
+    if (this.speech === null) {
+      return;
+    }
+
     this.speech.continuous = true;
     this.speech.interimResults = true;
-    this.speech.onresult = (event) => {
-      let lastresult = event.results[event.results.length-1];
-      let message = lastresult[0].transcript;
-      this.setState({
-        guess: message,
-      });
-      this.props.onguess(message);
-    };
+    this.speech.maxAlternatives = 10;
+    this.speech.onresult = this._onSpeechResult
     this.speech.onerror = (event) => {
       console.log("ERROR");
       console.log(event);
@@ -136,6 +213,23 @@ class RenderSpeech extends React.Component {
     }
     console.log("Capture Begin");
     this.speech.start();
+  }
+
+  _onSpeechResult(event) {
+    let lastresult = event.results[event.results.length-1];
+    console.log("{");
+    for (let i = 0; i < lastresult.length; i++) {
+      let guess = lastresult[i].transcript.split(' ').reverse()[0];
+      if (this.props.onguess) {
+        this.props.onguess(guess);
+      }
+    }
+    console.log("}");
+
+    let message = lastresult[0].transcript.split(' ').reverse()[0];
+    this.setState({
+      guess: message,
+    });
   }
 }
 
