7a8c8ad4d0ee3e462891cc6cab55be74f165f041
Peter Ruibal <ruibalp@gmail.com>
2016-01-28 22:00:23 -0800

    Actually works, kinda

--- main.js
+++ main.js
@@ -1,25 +1,80 @@
 'strict';
 
-// main.js
 var React = require('react');
 var ReactDOM = require('react-dom');
+var _ = require('lodash');
 
+
+const WORDS = [
+  ["square", "res/square.jpg", []],
+  ["octagon", "res/octagon.png", []],
+  ["oval", "res/oval.png", []],
+  ["moon", "res/moon.png", []],
+  ["pentagon", "res/pentagon.png", []],
+  ["rectangle", "res/rectangle.gif", []],
+  ["hexagon", "res/hexagon.gif", []],
+  ["parallelogram", "res/parallelogram.png", []],
+  ["heart", "res/heart.jpg", []],
+  ["star", "res/star.gif", []],
+  ["diamond", "res/diamond.jpg", []],
+  ["semicircle", "res/semicircle.gif", []],
+]
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      currentWord: null,
+    };
+    this.state.currentWord = this._pickWord();
+    this._onGuess = this._onGuess.bind(this);
+    this._speechRenderer = null;
   }
 
   render() {
     return (
-      <div>
-        <h1>Hello, world!</h1>
-        <RenderSpeech />
+      <div style={{textAlign: "center"}} >
+        <h1>What Shape is it?</h1>
+        <img 
+          style={{
+            width: "400px",
+          }}
+          src={this.state.currentWord[1]}
+        />
+        <RenderSpeech
+          onguess={this._onGuess}
+          ref={(o) => {this._speechRenderer = o}}
+        />
       </div>
     );
   }
+
+  _pickWord() {
+    let chosen = _.sample(WORDS);
+    if (this.state.currentWord === null) {
+      return chosen;
+    }
+
+    while (chosen[0] === this.state.currentWord[0]) {
+      chosen = _.sample(WORDS);
+    }
+    return chosen; 
+  }
+
+  _onGuess(guess) {
+    console.log("Guess: " + guess + "..." + this.state.currentWord[0]);
+    if (guess.toLowerCase().trim() === this.state.currentWord[0].toLowerCase().trim()) {
+      console.log("Correct!")
+      this.setState({
+        currentWord: this._pickWord(),
+      });
+      this._speechRenderer.clear();
+    }
+  }
 }
+
+
 
 class RenderSpeech extends React.Component {
 
@@ -27,8 +82,31 @@ class RenderSpeech extends React.Component {
     super(props);
     this.initSpeech();
     this.state = {
-      text: '',
+      guess: '',
+      allGuesses: [],
     };
+  }
+
+  static defaultProps = {
+    onguess: () => {},
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>{this.state.guess}</h2>
+      </div>
+    );
+  }
+
+  clear() {
+    this.speech.onresult = null;
+    this.speech.onend = null;
+    this.speech.stop();
+    this.initSpeech();
+    this.setState({
+      guess: '',
+    });
   }
 
   initSpeech() {
@@ -38,28 +116,23 @@ class RenderSpeech extends React.Component {
     this.speech.onresult = (event) => {
       let lastresult = event.results[event.results.length-1];
       let message = lastresult[0].transcript;
-      console.log("Message: " + message);
       this.setState({
-        text: message,
+        guess: message,
       });
+      this.props.onguess(message);
     };
     this.speech.onerror = (event) => {
       console.log("ERROR");
       console.log(event);
+      this.speech.stop()
     };
-    this.speech.audioend = (event) => {
+    this.speech.onend = (event) => {
       // Restart
       console.log("Capture Restarted");
       this.speech.start();
     }
     console.log("Capture Begin");
     this.speech.start();
-  }
-
-  render() {
-    return (
-      <div>{this.state.text}</div>
-    );
   }
 }
 
